927bb78a7ac00132f95b4155089ee98104d0e6a8
apbonillab <apbonillab@gmail.com>
2018-10-31 21:38:36 -0500

    modificando estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { FasesComponent } from './fases/fases.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LandingComponent } from './landing/landing.component';
 import { RecursosComponent } from './recursos/recursos.component';
-import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { CrearComponent } from './crear/crear.component';
 import { ErrorComponent } from './error/error.component';
 import { CoreModule } from './core/core.module';
@@ -32,6 +33,8 @@ import { CoreModule } from './core/core.module';
     FormsModule,
     ReactiveFormsModule,
     CoreModule.forRoot(),
+    NgbModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
