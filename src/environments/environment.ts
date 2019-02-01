a42ded3b203e2002b750f610822c1d05f40ccc94
michel <michelini19a@gmail.com>
2019-02-01 11:56:30 -0500

    environment 8080

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-   url: 'http://127.0.0.1:8000/'
+   url: 'http://127.0.0.1:8080/'
   //url: 'https://resources-admin-conectate.herokuapp.com/'
 };
 
